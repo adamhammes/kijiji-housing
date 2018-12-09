fd10947ebc7e12ff76b5ef7535859febd81cd789
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-09 13:15:05 -0500

    Make API call in offers display

--- src/templates/offers-display.js
+++ src/templates/offers-display.js
@@ -1,8 +1,19 @@
+import { withPrefix } from 'gatsby';
 import React from 'react'
 
 export default class OffersDisplay extends React.Component {
+    componentDidMount() {
+        const { city, ad_type } = this.props.pathContext;
+        const apiPath = withPrefix(`/api/${city.id}-${ad_type.id}.json`);
+
+        fetch(apiPath)
+            .then(res => res.json())
+            .then(result => {
+                this.setState({ allOffers: result.offers })
+            });
+    }
+
     render() {
-        console.log(this.props);
         return (
             <div>
                 {this.props.pathContext.city.id}
