aa2f2d9dde50c1c5aa7a20af89282a97bec45acf
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-24 22:30:35 -0600

    Add maxWidth on front page

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,9 +31,22 @@ const Layout = ({ children, locale, renderHeader = true }) => {
             <html lang={locale.language} />
           </Helmet>
           {renderHeader ? (
-            <Header siteTitle={data.site.siteMetadata.title} />
-          ) : null}
-          {children}
+            <>
+              <Header siteTitle={data.site.siteMetadata.title} />
+              <div
+                style={{
+                  margin: "0 auto",
+                  maxWidth: 960,
+                  padding: "0px 1.0875rem 1.45rem",
+                  paddingTop: 0,
+                }}
+              >
+                {children}
+              </div>
+            </>
+          ) : (
+            children
+          )}
         </>
       )}
     />
--- src/pages/index.js
+++ src/pages/index.js
@@ -44,43 +44,41 @@ class IndexPage extends React.Component {
         query={query}
         render={data => (
           <Layout locale={locale}>
-            <div className="front-page">
-              <form onChange={this.onChange}>
-                <h2>{locale.messages.frontPage.lookingFor}</h2>
-                <div className="options-container">
-                  {data.apiJson.ad_types.map(a => (
-                    <label key={a.id}>
-                      <input
-                        key={a.id}
-                        type="radio"
-                        name="ad_type"
-                        value={a.id}
-                        defaultChecked={this.state.ad_type === a.id}
-                      />
-                      {locale.messages.frontPage[a.id]}
-                    </label>
-                  ))}
-                </div>
-                <h2>{locale.messages.frontPage.in}</h2>
-                <div className="options-container">
-                  {data.apiJson.cities.map(c => (
-                    <label key={c.id}>
-                      <input
-                        key={c.id}
-                        type="radio"
-                        name="city"
-                        value={c.id}
-                        defaultChecked={this.state.city === c.id}
-                      />
-                      {locale.messages.cities[c.id]}
-                    </label>
-                  ))}
-                </div>
-                <Link to={this.nextLink()}>
-                  {locale.messages.frontPage.letsGo}
-                </Link>
-              </form>
-            </div>
+            <form onChange={this.onChange}>
+              <h2>{locale.messages.frontPage.lookingFor}</h2>
+              <div className="options-container">
+                {data.apiJson.ad_types.map(a => (
+                  <label key={a.id}>
+                    <input
+                      key={a.id}
+                      type="radio"
+                      name="ad_type"
+                      value={a.id}
+                      defaultChecked={this.state.ad_type === a.id}
+                    />
+                    {locale.messages.frontPage[a.id]}
+                  </label>
+                ))}
+              </div>
+              <h2>{locale.messages.frontPage.in}</h2>
+              <div className="options-container">
+                {data.apiJson.cities.map(c => (
+                  <label key={c.id}>
+                    <input
+                      key={c.id}
+                      type="radio"
+                      name="city"
+                      value={c.id}
+                      defaultChecked={this.state.city === c.id}
+                    />
+                    {locale.messages.cities[c.id]}
+                  </label>
+                ))}
+              </div>
+              <Link to={this.nextLink()}>
+                {locale.messages.frontPage.letsGo}
+              </Link>
+            </form>
           </Layout>
         )}
       />
