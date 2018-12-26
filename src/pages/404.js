00951719ff97ea00bea61952b98714f338fbc7c2
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-26 14:58:54 -0600

    Localize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Link } from "gatsby";
 
 const NotFoundPage = ({ pageContext }) => {
   const { locale } = pageContext;
 
+  const homePath = `/${locale.language}/`;
+
   return (
     <Layout locale={locale}>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>{locale.messages.notFound.doesNotExist}</h1>
+      <Link to={homePath}>{locale.messages.notFound.returnToHome}</Link>
     </Layout>
   );
 };
