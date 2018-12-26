3f0574fbb19e5199b29cffc48d86f940c2d27f84
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-26 09:59:01 -0600

    Convert layout styles to sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
-import "./layout.css";
+import "./layout.scss";
 import "./app.css";
 
 const Layout = ({ children, locale, renderHeader = true }) => {
