6b12c90aff97c8b4ca4371ef43a19a9d849d7a98
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-31 15:42:04 -0600

    Fix bug with passing link props

--- src/components/lib.js
+++ src/components/lib.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { LocaleContext } from "./locale-context";
 import { Link } from "gatsby";
 
-const LocalizedLink = ({ to, children, linkProps }) => {
+const LocalizedLink = ({ to, children, ...linkProps }) => {
   const locale = useContext(LocaleContext);
 
   const localizedTo = `/${locale.language}/${to}`;
