d8698b6db3fec867cd4a438d2876163221d4a3a0
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-31 15:42:27 -0600

    Use localization helpers in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,35 @@
-import { Link } from "gatsby";
-import React, { useContext } from "react";
-import { LocaleContext } from "./locale-context";
+import React from "react";
 import LanguageSwitcher from "./language-switcher";
+import { LocalizedLink } from "./lib";
 
-const Header = ({ siteTitle }) => {
-  const lang = useContext(LocaleContext).language;
-
-  return (
-    <header
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: "#EA5500",
+      marginBottom: "1.45rem",
+    }}
+  >
+    <div
       style={{
-        background: "#EA5500",
-        marginBottom: "1.45rem",
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "1.45rem 1.0875rem",
       }}
     >
-      <div
-        style={{
-          margin: "0 auto",
-          maxWidth: 960,
-          padding: "1.45rem 1.0875rem",
-        }}
-      >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to={`/${lang}`}
-            style={{
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-        <LanguageSwitcher className="header-language-switcher" />
-      </div>
-    </header>
-  );
-};
+      <h1 style={{ margin: 0 }}>
+        <LocalizedLink
+          to="/"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          {siteTitle}
+        </LocalizedLink>
+      </h1>
+      <LanguageSwitcher className="header-language-switcher" />
+    </div>
+  </header>
+);
 
 export default Header;
