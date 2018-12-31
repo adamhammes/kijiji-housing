2e66b21c876ae6f5c8b576b70b04d385e413071e
Adam Hammes <ahammes@cortexmedia.ca>
2018-12-31 11:57:22 -0600

    Add analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
+import galite from "ga-lite";
+
+const onClientEntry = () => {
+  galite("create", "UA-113910402-1", "auto");
+  galite("send", "pagview");
+};
+
+export { onClientEntry };
 export { wrapPageElement } from "./src/components/locale-context";
