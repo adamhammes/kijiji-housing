bc8a3045a1e41a351eec53ab388bde5ddb92e52e
Adam Hammes <ahammes@cortexmedia.ca>
2019-01-01 00:40:50 -0600

    Make the index page more idiomatic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import React, { useState } from "react";
+import { graphql } from "gatsby";
 
 import "./front-page.scss";
 import { LocalizedLink, Message } from "../components/lib";
 
-const query = graphql`
+export const query = graphql`
   {
     apiJson {
       cities {
@@ -17,75 +17,58 @@ const query = graphql`
   }
 `;
 
-class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
+const IndexPage = ({ data }) => {
+  const { cities, ad_types } = data.apiJson;
 
-    const defaultState = {
-      city: "quebec",
-      ad_type: "rent",
-    };
+  const [currentCity, setCity] = useState(cities[0]);
+  const [currentAdType, setAdType] = useState(ad_types[0]);
 
-    this.state = defaultState;
-
-    this.onChange = this.onChange.bind(this);
-  }
-
-  render() {
-    return (
-      <StaticQuery
-        query={query}
-        render={data => (
-          <form onChange={this.onChange}>
-            <h2>
-              <Message>frontPage.lookingFor</Message>
-            </h2>
-            <div className="options-container">
-              {data.apiJson.ad_types.map(a => (
-                <label key={a.id}>
-                  <input
-                    key={a.id}
-                    type="radio"
-                    name="ad_type"
-                    value={a.id}
-                    defaultChecked={this.state.ad_type === a.id}
-                  />
-                  <Message>{`frontPage.${a.id}`}</Message>
-                </label>
-              ))}
-            </div>
-            <h2>
-              <Message>frontPage.in</Message>
-            </h2>
-            <div className="options-container">
-              {data.apiJson.cities.map(c => (
-                <label key={c.id}>
-                  <input
-                    key={c.id}
-                    type="radio"
-                    name="city"
-                    value={c.id}
-                    defaultChecked={this.state.city === c.id}
-                  />
-                  <Message>{`cities.${c.id}`}</Message>
-                </label>
-              ))}
-            </div>
-            <LocalizedLink
-              className="toOfferPage"
-              to={`/${this.state.city}/${this.state.ad_type}/`}
-            >
-              <Message>frontPage.letsGo</Message>
-            </LocalizedLink>
-          </form>
-        )}
-      />
-    );
-  }
-
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-}
+  return (
+    <form>
+      <h2>
+        <Message>frontPage.lookingFor</Message>
+      </h2>
+      <div className="options-container">
+        {ad_types.map(a => (
+          <label key={a.id}>
+            <input
+              key={a.id}
+              type="radio"
+              name="ad_type"
+              value={a.id}
+              defaultChecked={currentAdType.id === a.id}
+              onChange={() => setAdType(a)}
+            />
+            <Message>{`frontPage.${a.id}`}</Message>
+          </label>
+        ))}
+      </div>
+      <h2>
+        <Message>frontPage.in</Message>
+      </h2>
+      <div className="options-container">
+        {cities.map(c => (
+          <label key={c.id}>
+            <input
+              key={c.id}
+              type="radio"
+              name="city"
+              value={c.id}
+              checked={currentCity.id === c.id}
+              onChange={() => setCity(c)}
+            />
+            <Message>{`cities.${c.id}`}</Message>
+          </label>
+        ))}
+      </div>
+      <LocalizedLink
+        className="toOfferPage"
+        to={`/${currentCity.id}/${currentAdType.id}/`}
+      >
+        <Message>frontPage.letsGo</Message>
+      </LocalizedLink>
+    </form>
+  );
+};
 
 export default IndexPage;
