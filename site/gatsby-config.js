f7f9fe282ce5162ef69c11cda0a91649bbe4e7d8
Adam Hammes <ahammes@cortexmedia.ca>
2019-06-24 19:32:34 -0500

    Delete commented-out config

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelistPatterns: [/.*leaflet.*/, /.*marker.*/, /.*gatsby.*/], // Don't remove this selector
+        whitelistPatterns: [/leaflet/, /marker/, /gatsby/], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
@@ -62,8 +62,5 @@ module.exports = {
         },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 };
